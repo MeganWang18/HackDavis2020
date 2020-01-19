1bada58fe164340f3ed492f726b4d9376af7ef5d
Megan Wang <mxwang@ucdavis.edu>
2020-01-18 17:02:29 -0800

    Add UI STUFFFF

---
+++ MEO/ButtonScript.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonScript.swift
+//  MEO
+//
+//  Created by megan on 1/18/20.
+//  Copyright © 2020 MAMA. All rights reserved.
+//
+
+import UIKit
+
+class ButtonScript: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
